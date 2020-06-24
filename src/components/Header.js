d4eb10a133c748f6f863727293c526fd422ada78
Miguel Somohano <miguel.spinis@gmail.com>
2020-06-24 08:50:07 -0500

    Creando styled component Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import styled from '@emotion/styled'; //Libreria emotion, se usa para el css en style components VER DOCUMENTACION
+
+// styled componentes: Despues de styled.<elemento html> y en un template string escribimos el css
+const ContenedorHeader = styled.header`
+    background-color: #26C6DA;
+    padding: 10px;
+    font-weight: bold;
+    color: #FFFFFF;
+`;
+
+const TextoHeader = styled.h1`
+    font-size: 2rem;
+    margin: 0;
+    font-family: 'Slabo 27px', serif;
+    text-align: center; 
+`;
 
 const Header = ({ titulo }) => {
     return ( 
-        <header>
-            <h1>{ titulo }</h1>
-        </header>
+        <ContenedorHeader>
+            <TextoHeader>{ titulo }</TextoHeader>
+        </ContenedorHeader>
      );
 }
  
