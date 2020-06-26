efc6aa5dd0a4ce58b0ccc17e7816d628ec5e696b
Miguel Somohano <miguel.spinis@gmail.com>
2020-06-26 08:54:28 -0500

    Agregando styled component button en Formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -23,6 +23,25 @@ const InputRadio =  styled.input`
     margin: 0 1rem;
 `;
 
+const Button = styled.button`
+    background-color: #00838F;
+    font-size: 16px;
+    width: 100%;
+    padding: 1rem;
+    color: #fff;
+    text-transform: uppercase;
+    font-weight: bold;
+    border: none;
+    transition: backgroud-color .3s ease;
+    margin-top: 2rem;
+
+    /* Sintaxis tipo Sas  */
+    &:hover {
+        background-color: #26C6DA;
+        cursor: pointer;
+    }
+`;
+
 
 const Formulario = () => {
     return ( 
@@ -67,7 +86,7 @@ const Formulario = () => {
                     value="completo"
                 /> Completo
             </Campo>
-            <button type="button">Cotizar</button>
+            <Button type="button">Cotizar</Button>
         </form>
      );
 }
